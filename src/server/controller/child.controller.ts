3c6865542d40729eee1971bee41d5d53ba4e43d4
Christophe DUGAS DU VILLARD <c.dugas.du.villard@FIDUCIAL.DOM>
2018-02-27 08:53:53 +0100

    modification sur le child controller

--- src/server/controller/child.controller.ts
+++ src/server/controller/child.controller.ts
@@ -1,6 +1,7 @@
 
 import {createConnection} from "typeorm";
 import { Router, RequestHandler, Response, Next, Request, Express } from 'express';  
+import { BodyParser } from 'body-parser';  
 
 import {Child} from "../entity/child.entity";
 
@@ -29,11 +30,26 @@ export class ChildController {
         }).catch(error => console.log(error));       
     }
     
-    /*
     static PostChild(req: Request, res: Response)
     {
+        if (req.
+        createConnection().then(async connection => {
 
-    }*/
+    
+            let aChild = new Child();
+            aChild.familyname = "Dugas";
+            aChild.firstname = "Christophe"
+      
+            let childRepository = connection.getRepository(Child);
+      
+            await childRepository.save(aChild);
+            console.log("aChild has been saved");
+      
+            let someChilds = await childRepository.find();
+            console.log("All photos from the db: ", someChilds);
+      
+        }).catch(error => console.log(error));
+    }
 
     // DELETE, PUT (update)
 
